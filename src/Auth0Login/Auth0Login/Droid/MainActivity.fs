9ee634f7d94ca8318a632df47ee6b93ecb3c1e4d
Biff <jamessdixon@gmail.com>
2019-02-26 15:50:38 -0500

    initial commit

---
+++ src/Auth0Login/Auth0Login/Droid/MainActivity.fs
@@ -0,0 +1,27 @@
+﻿namespace Auth0Login.Droid
+open System
+
+open Android.App
+open Android.Content
+open Android.Content.PM
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.OS
+open Xamarin.Forms.Platform.Android
+
+type Resources = Auth0Login.Droid.Resource
+
+[<Activity (Label = "Auth0Login.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit FormsAppCompatActivity()
+
+    override this.OnCreate (bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+
+        this.LoadApplication (new Auth0Login.App ())
